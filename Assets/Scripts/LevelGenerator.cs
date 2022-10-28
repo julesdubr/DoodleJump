dfac23344bdc2285f1dcbe833257b8925a1794f3
julesdubr <jules.dubreuil@outlook.fr>
2022-10-28 10:34:11 +0200

    implemented infinite generation

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -11,38 +11,44 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private List<GameObject> obstaclesList;
     [SerializeField] private float[] obstaclesProbs = { 0.3f, 0.6f, 0.9f, 1f };
 
-    [SerializeField] private int numberOfPlatforms = 200;
     [SerializeField] private float levelWidth = 2.5f;
     [SerializeField] private float minY = .5f;
     [SerializeField] private float maxY = 1.5f;
 
     private Vector3 spawnPosition = new Vector3(0f, -3f, 0f);
+    [SerializeField] private float spawnYLimit = 1.2f;
+    private Collider2D hitCollider;
 
     void Start()
     {
-        for (int i = 0; i < numberOfPlatforms; i++)
-        {
-            // Spawn obstacle
-            if (Random.Range(0f, 1f) <=  obstacleSpawnProb)
-            {
-                GameObject obstacle = GetRandomPrefab(obstaclesList, obstaclesProbs);
+        while (Camera.main.WorldToViewportPoint(spawnPosition).y < spawnYLimit)
+            SpawnObstaclesPlatforms();
+    }
 
-                // Randomize position
-                RandomizeSpawn(-.1f, .2f);
-                Instantiate(obstacle, spawnPosition, Quaternion.identity);
-            }
-
-            // Spawn a random platform
-            GameObject platform = GetRandomPrefab(platformsList, platformsProbs);
-
-            // Randomize position
-            RandomizeSpawn(minY, maxY);
-            Instantiate(platform, spawnPosition, Quaternion.identity);
+    void Update()
+    {
+        if (Camera.main.WorldToViewportPoint(spawnPosition).y < spawnYLimit) {
+            SpawnObstaclesPlatforms();
         }
     }
 
-    void Update() {
-        Vector3 position = Camera.main.WorldToViewportPoint(spawnPosition);
+    private void SpawnObstaclesPlatforms() {
+        // Spawn obstacle
+        if (Random.Range(0f, 1f) <= obstacleSpawnProb)
+        {
+            GameObject obstacle = GetRandomPrefab(obstaclesList, obstaclesProbs);
+
+            // Randomize position
+            RandomizeSpawn(-.1f, .2f);
+            Instantiate(obstacle, spawnPosition, Quaternion.identity);
+        }
+
+        // Spawn a random platform
+        GameObject platform = GetRandomPrefab(platformsList, platformsProbs);
+        
+        // Randomize position
+        RandomizeSpawn(minY, maxY);
+        Instantiate(platform, spawnPosition, Quaternion.identity);
     }
 
     private GameObject GetRandomPrefab(List<GameObject> prefabList, float[] probs)
@@ -57,13 +63,12 @@ public class LevelGenerator : MonoBehaviour
 
     void RandomizeSpawn(float minY, float maxY)
     {
-        // spawnPosition.x = Random.Range(-_levelWidth, _levelWidth);
-        // marche pas
-        do
+        do // pas l'impression que ca marche a chaque fois
         {
             spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+            hitCollider = Physics2D.OverlapBox(spawnPosition, new Vector2(1.2f, .3f), 0f);
         }
-        while (Physics2D.OverlapBox(spawnPosition, new Vector2(1.2f, .3f), 0f) != null);
+        while (hitCollider != null);
         spawnPosition.y += Random.Range(minY, maxY);
     }
 }
